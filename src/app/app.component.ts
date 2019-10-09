09d65201f5d21d6edc9eafb4ebde270859fd382f
jpdcorte <jensjoris.decorte@ugent.be>
2019-10-09 18:42:53 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'Raad het getal';
+  randomNumber = Math.floor(Math.random()*100);
+  aantalGokken = 10;
+  hint = "";
+
+  onEnter(value: string) {
+    if (isNaN(Number(value))) {
+      this.hint = "Je moet een nummer ingeven..";
+    } else {
+      const inputNumber: number = Number(value);
+      if (this.randomNumber == inputNumber) {
+        this.hint = "Je hebt gewonnen!";
+      } else if (this.randomNumber < inputNumber) {
+        this.hint = "Kies een lager nummer";
+      } else {
+        this.hint = "Ga voor een hoger nummer";
+      }
+      this.aantalGokken -= 1;
+    }
+  }
 }
