e17af8c67db7356059fa1b826d828586ca4ac794
jpdcorte <jensjoris.decorte@ugent.be>
2019-10-10 20:40:32 +0200

    styled application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MaterialModule} from './material.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MaterialModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
-  imports: [MatButtonModule, ],
-  exports: [MatButtonModule, ]
+  imports: [MatButtonModule, MatInputModule, MatProgressBarModule,],
+  exports: [MatButtonModule, MatInputModule, MatProgressBarModule,]
 })
 export class MaterialModule {}
